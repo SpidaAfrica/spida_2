2902404177189a3dc81e77d8ccdb4917a110b2ff
jbuittechservices <jbuittechservices@gmail.com>
2026-02-26 16:06:45 +0100

    Update BookingCard.jsx

--- src/pages/spi-tractors/components/bookings/BookingCard.jsx
+++ src/pages/spi-tractors/components/bookings/BookingCard.jsx
@@ -1,23 +1,20 @@
 export default function BookingCard({ data }) {
-    return (
-      <div className="bk-card">
-        <div className="bk-card-top">
-          <div>
-            <div className="bk-id">{data.id}</div>
-            <div className="bk-mini">{data.farmers}</div>
-          </div>
-  
-          <button className="bk-dots" title="More">
-            ⋮
-          </button>
+  return (
+    <div className="bk-card">
+      <div className="bk-card-top">
+        <div>
+          <div className="bk-id">{data.id}</div>
+          <div className="bk-mini">{data.farmers}</div>
         </div>
-  
-        <div className="bk-loc">{data.location}</div>
-        <div className="bk-service">{data.service}</div>
-  
-        <div className="bk-tag">{data.tag}</div>
-        <div className="bk-time">{data.time}</div>
+
+        <button className="bk-dots" title="More">⋮</button>
       </div>
-    );
-  }
-  
+
+      <div className="bk-loc">{data.location}</div>
+      <div className="bk-service">{data.service}</div>
+
+      <div className="bk-tag">{data.tag}</div>
+      <div className="bk-time">{data.time}</div>
+    </div>
+  );
+}
