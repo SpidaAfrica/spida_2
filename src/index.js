2a502549ed7383c1c71a712095972192d807b0e8
jbuittechservices <jbuittechservices@gmail.com>
2026-04-21 01:17:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-
+import { registerPWA } from "./pages/spi-tractors/registerPWA";
 
 
 
